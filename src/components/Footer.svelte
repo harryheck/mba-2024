950cc4af1b1a3ddad8f232125b8751e36d951f4d
Roman Schweikert <roman.schweikert@gmail.com>
2024-03-25 04:24:25 +0100

    bring on the code

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,25 +1,50 @@
 <footer
-    class="py-20 sm:py-12 bg-black border-t border-solid border-violet-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
+    class="py-20 sm:py-12 bg-black border-t border-solid border-blue-950 flex flex-col gap-4 sm:gap-8 justify-center items-center"
 >
     <p class="px-4 py-2 bg-white text-slate-950 font-medium">
-        Connect with me &darr;
+        Connect with us &darr;
     </p>
 
-    <div class="grid grid-cols-3 gap-4 md:gap-4 items-center justify-center mx-auto">
-        <a href="https://www.instagram.com/romansweik/" target="_blank" rel="noopener noreferrer">
-            <i class="fab fa-instagram fa-2x"></i>
+    <div
+        class="grid grid-cols-4 gap-4 md:gap-4 items-center justify-center mx-auto"
+    >
+        <a
+            href="https://www.instagram.com/mybestantic/"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <i class="fab fa-instagram fa-2x hover:text-pink-400 duration-200"
+            ></i>
         </a>
 
-        <a href="https://www.threads.net/@romansweik" target="_blank" rel="noopener noreferrer">
-            <i class="fab fa-threads fa-2x"></i>
+        <a
+            href="https://www.youtube.com/@mybestantic"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <i class="fab fa-youtube fa-2x hover:text-red-400 duration-200"></i>
         </a>
 
-        <a href="https://www.youtube.com/@romansweik" target="_blank" rel="noopener noreferrer">
-            <i class="fab fa-youtube fa-2x"></i>
+        <a
+            href="https://www.threads.net/@mybestantic"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <i class="fab fa-threads fa-2x hover:text-gray-400 duration-200"
+            ></i>
+        </a>
+
+        <a
+            href="https://www.facebook.com/mybestantic"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <i class="fab fa-facebook fa-2x hover:text-blue-400 duration-200"
+            ></i>
         </a>
     </div>
     <p class="text-xs">
         <i class="fa-regular fa-copyright"></i>
-        Roman Schweikert 2024 🤍
+        My Best Antic 2024 🤍
     </p>
 </footer>
